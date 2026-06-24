5deeb28cf03003304acc9615c668d64cb4cb2905
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-06-24 08:13:27 +0500

    .

---
+++ app/_components/restaurantLogin.js
@@ -0,0 +1,8 @@
+const RestaurantLogin=()=>{
+    return(
+        <>
+        <h1>Login component</h1>
+        </>
+    )
+}
+export default RestaurantLogin;
---
+++ app/_components/restaurantSignUp.js
@@ -0,0 +1,9 @@
+const RestaurantSignUp=()=>{
+    return(
+        <>
+        <h1>SignUp component</h1>
+        
+        </>
+    )
+}
+export default RestaurantSignUp;
