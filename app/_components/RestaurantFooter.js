9b0d130acb7e5b4f28f4a91e86b118597a8677ef
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-06-26 12:17:36 +0500

    .

--- app/_components/RestaurantFooter.js
+++ app/_components/RestaurantFooter.js
@@ -1,7 +1,7 @@
 const RestaurantFooter=()=>{
     return(
         <div className="footer-wrapper">
-            <p>All Right Reserved By Resto App
+            <p>All Rights Reserved By Resto App
             </p>
         </div>
     )
---
+++ app/lib/db.js
@@ -0,0 +1,1 @@
+export const connectioStr="mongodb+srv://rimshashahzadi806_db_user:<db_password>@cluster0.0phlcpn.mongodb.net/?appName=Cluster0";
