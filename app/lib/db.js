052fac5145a98d2f36de29ebc4cbc68996bca7fe
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-07-06 06:57:00 +0500

    .

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,10 +1,2 @@
 const {username,password} = process.env
-// export const connectioStr="mongodb+srv://rimshashahzadi806_db_user:<db_password>@cluster0.0phlcpn.mongodb.net/?appName=Cluster0";
-// export const connectionStr="mongodb+srv://"+username+":"+password+"@cluster0.0phlcpn.mongodb.net/resto-project?appName=Cluster0";
-// export const connectionStr="mongodb+srv://"+username+":"+password+"@cluster0.y2gfapn.mongodb.net/?restoDB=Cluster0"
-
-// export const  connectionStr  = "mongodb+srv://"+username+":"+password+"@cluster0.abcde.mongodb.net/restodb";
-
-// export const connectionStr =
-// "mongodb+srv://username:password@cluster0.xxxxx.mongodb.net/restodb?retryWrites=true&w=majority";
-export const cnnectinStr="mongodb+srv://"+username+":+"password+"@cluster0.y2gfapn.mongodb.net/"
+export const cnnectinStr="mongodb+srv://"+username+":"+password+"@cluster0.y2gfapn.mongodb.net/?appName=Cluster0"
