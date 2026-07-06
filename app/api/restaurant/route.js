f683306f1ba34c7fe07eac01fe4ab78662fdd08d
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-07-06 06:07:45 +0500

    .

--- app/api/restaurant/route.js
+++ app/api/restaurant/route.js
@@ -1,15 +1,15 @@
-// import { restaurantSchema } from "@/app/lib/restaurantModel";
-// import { connectionStr } from "@/app/lib/db";
-// import { NextResponse } from "next/server";
-// import mongoose  from "mongoose";
-// export async function GET(){
-//     // await mongoose.connect(connectionStr,{useNewUrlParser:true})
-//     await mongoose.connect(connectionStr, { useNewUrlParser: true })
-//     await mongoose.connect(connectionStr)
-//     const data= await restaurantSchema.find()
-//     console.log(data);
-// return NextResponse.json({result:true})
-// }
+import { restaurantSchema } from "@/app/lib/restaurantModel";
+import { connectionStr } from "@/app/lib/db";
+import { NextResponse } from "next/server";
+import mongoose  from "mongoose";
+export async function GET(){
+    // await mongoose.connect(connectionS.tr,{useNewUrlParser:true})
+    await mongoose.connect(connectionStr, { useNewUrlParser: true })
+    await mongoose.connect(connectionStr)
+    const data= await restaurantSchema.find()
+    console.log(data);
+return NextResponse.json({result:true})
+}
 
 import { restaurantScheme } from "@/app/lib/restaurantModel";
 import { connectionStr } from "@/app/lib/db";
