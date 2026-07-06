78e4f9341759ea84908c26772d3845a06ce562bc
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-07-06 06:07:18 +0500

    .

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,9 +1,9 @@
 const {username,password} = process.env
-export const connectioStr="mongodb+srv://rimshashahzadi806_db_user:<db_password>@cluster0.0phlcpn.mongodb.net/?appName=Cluster0";
-export const connectionStr="mongodb+srv://"+username+":"+password+"@cluster0.0phlcpn.mongodb.net/resto-project?appName=Cluster0";
+// export const connectioStr="mongodb+srv://rimshashahzadi806_db_user:<db_password>@cluster0.0phlcpn.mongodb.net/?appName=Cluster0";
+// export const connectionStr="mongodb+srv://"+username+":"+password+"@cluster0.0phlcpn.mongodb.net/resto-project?appName=Cluster0";
 export const connectionStr="mongodb+srv://"+username+":"+password+"@cluster0.y2gfapn.mongodb.net/?restoDB=Cluster0"
 
-export const  connectionStr  = "mongodb+srv://"+username+":"+password+"@cluster0.abcde.mongodb.net/restodb";
+// export const  connectionStr  = "mongodb+srv://"+username+":"+password+"@cluster0.abcde.mongodb.net/restodb";
 
-export const connectionStr =
-"mongodb+srv://username:password@cluster0.xxxxx.mongodb.net/restodb?retryWrites=true&w=majority";
+// export const connectionStr =
+// "mongodb+srv://username:password@cluster0.xxxxx.mongodb.net/restodb?retryWrites=true&w=majority";
