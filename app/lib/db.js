90c4122f483fbd9d08eb2a65281e33a32f69cbd2
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-07-07 09:33:09 +0500

    .

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,2 +1,8 @@
-const {username,password} = process.env
-export const connectinStr="mongodb+srv://"+username+":"+password+"@cluster0.y2gfapn.mongodb.net/?appName=Cluster0"
+// const {username,password} = process.env
+// export const connectinStr="mongodb+srv://"+username+":"+password+"@cluster0.y2gfapn.mongodb.net/?appName=Cluster0"
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+export const connectionStr =
+  `mongodb+srv://${username}:${password}@cluster0.y2gfapn.mongodb.net/restodb?retryWrites=true&w=majority`;
