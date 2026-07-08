fa1cba77ef7cd555c2eb60188e1d798ee204f097
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-07-08 08:27:53 +0500

    .

--- app/api/restaurant/route.js
+++ app/api/restaurant/route.js
@@ -4,7 +4,8 @@ import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 
 export async function GET() {
-    await mongoose.connect(connectionStr,{useNewUrlParser:true});
+    // await mongoose.connect(connectionStr,{useNewUrlParser:true});
+    await mongoose.connect(connectionStr);
     const data = await restaurantScheme.find();
     console.log(data);
 return NextResponse.json({result: data})
