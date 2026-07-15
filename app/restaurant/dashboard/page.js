e03806ae0d00a76503d929dc2a71b31786f7d245
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-07-15 08:14:15 +0500

    .

--- app/restaurant/dashboard/page.js
+++ app/restaurant/dashboard/page.js
@@ -1,8 +1,14 @@
+"use client"
+import RestaurantHeader from "@/app/_components/RrestaurantHeader";
+import './../style.css';
 const Dashboard=()=>{
     return(
         <div>
+            <RestaurantHeader />
             <h1>Welcome To Dashboard</h1>
         </div>
     )
 }
-export default Dashboard;
+export default Dashboard
+export default Dashboard
+export default Dashboard
